e7ec949394647465ee5ec40c558204203a7fdcb1
Sammy Weller <sammymermell@gmail.com>
2023-08-28 10:23:00 -0400

    load data offline

--- src/api.js
+++ src/api.js
@@ -53,6 +53,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    return events?JSON.parse(events):[];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -61,8 +66,9 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
-    } else return null; 
+    } else return null;
   }
 };
 
