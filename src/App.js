3968366a9906aca7d17990120b0a72dcd70b33cc
Sammy Weller <sammymermell@gmail.com>
2023-08-29 09:19:45 -0400

    added events chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import CityEventsChart from './components/CityEventsChart';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
@@ -45,6 +46,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
@@ -52,6 +54,7 @@ const App = () => {
         setInfoAlert={setInfoAlert} />
       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}
  />
+ <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
